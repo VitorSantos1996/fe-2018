fa2fc2474681875379f8b25061cb526224bdcd6b
Kaichiro <kcfg23@gmail.com>
2018-11-10 15:39:14 -0200

    Ajustando fundo de mapa do site

--- docs/js/js.js
+++ docs/js/js.js
@@ -4,31 +4,42 @@ function container1() {
     document.getElementsByClassName('container')[0].style.display = 'none';
     document.getElementsByClassName('nav')[0].style.visibility = 'visible';
     document.getElementsByClassName('containeri')[0].style.display = 'inline-block';
+    document.getElementsByTagName('li')[0].style.backgroundColor = "#8C8C8C";
 }
 
 function containern() {
     document.getElementsByClassName('containeri')[0].style.display = 'none';
     document.getElementsByClassName('containern')[0].style.display = 'inline-block';
+    document.getElementsByTagName('li')[0].style.backgroundColor = "#CCCCCC";
+    document.getElementsByTagName('li')[1].style.backgroundColor = "#8C8C8C";
 }
 
 function containerd() {
     document.getElementsByClassName('containern')[0].style.display = 'none';
     document.getElementsByClassName('containerd')[0].style.display = 'inline-block';
+    document.getElementsByTagName('li')[1].style.backgroundColor = "#CCCCCC";
+    document.getElementsByTagName('li')[2].style.backgroundColor = "#8C8C8C";
 }
 
 function containere() {
     document.getElementsByClassName('containerd')[0].style.display = 'none';
     document.getElementsByClassName('containere')[0].style.display = 'inline-block';
+    document.getElementsByTagName('li')[2].style.backgroundColor = "#CCCCCC";
+    document.getElementsByTagName('li')[3].style.backgroundColor = "#8C8C8C";
 }
 
 function containerc() {
     document.getElementsByClassName('containere')[0].style.display = 'none';
     document.getElementsByClassName('containerc')[0].style.display = 'inline-block';
+    document.getElementsByTagName('li')[3].style.backgroundColor = "#CCCCCC";
+    document.getElementsByTagName('li')[4].style.backgroundColor = "#8C8C8C";
 }
 
 function containerv() {
     document.getElementsByClassName('containerc')[0].style.display = 'none';
     document.getElementsByClassName('containerv')[0].style.display = 'inline-block';
+    document.getElementsByTagName('li')[4].style.backgroundColor = "#CCCCCC";
+    document.getElementsByTagName('li')[5].style.backgroundColor = "#8C8C8C";
 }
 
 function finalizar() {
@@ -38,4 +49,11 @@ function finalizar() {
     document.getElementsByClassName('container')[0].style.display = 'inline-block';
     document.getElementsByClassName('nav')[0].style.visibility = 'hidden';
     document.getElementsByClassName('containeri')[0].style.display = 'none';
+    document.getElementsByTagName('li')[0].style.backgroundColor = "#F2F2F2";
+    document.getElementsByTagName('li')[1].style.backgroundColor = "#F2F2F2";
+    document.getElementsByTagName('li')[2].style.backgroundColor = "#F2F2F2";
+    document.getElementsByTagName('li')[3].style.backgroundColor = "#F2F2F2";
+    document.getElementsByTagName('li')[4].style.backgroundColor = "#F2F2F2";
+    document.getElementsByTagName('li')[5].style.backgroundColor = "#F2F2F2";
+    document.getElementsByTagName('li')[6].style.backgroundColor = "#F2F2F2";
 }
